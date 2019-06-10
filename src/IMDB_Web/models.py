190b64df3254e79dcff3534edf0840e151b069c7
sify <sifyk@seedolabs.com>
2019-06-10 16:36:25 +0530

    database

--- src/IMDB_Web/models.py
+++ src/IMDB_Web/models.py
@@ -61,4 +61,137 @@ class Movie(db.Model):
             'self.description':self.description
         }
 
+class Actor(db.Model):
+    __tablename__ = 'Actors'
 
+    id = db.Column(db.Integer, Sequence('actors_id_seq'), primary_key=True)
+    profileid =db.Column(db.Integer,db.ForeignKey('Profiles.id',ondelete="CASCADE"))
+    note=db.Column(db.Text())
+
+    def __init__(self, id, profileid, note):
+        self.id = id
+        self.profileid = profileid
+        self.note= note
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'profileid': self.profileid,
+            'note': self.note,
+        }
+
+
+class MovieActor(db.Model):
+    __tablename__ = 'MovieActors'
+
+    id = db.Column(db.Integer, Sequence('movieactors_id_seq'), primary_key=True)
+    actorid = db.Column(db.Integer, db.ForeignKey('Actors.id',ondelete="CASCADE"))
+    movieid = db.Column(db.Integer, db.ForeignKey('Movies.id',ondelete="CASCADE"))
+
+    def __init__(self, id, actorid, movieid):
+        self.id = id
+        self.actorid = actorid
+        self.movieid = movieid
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'actorid': self.actorid,
+            'movieid': self.movieid,
+        }
+
+
+class Director(db.Model):
+    __tablename__ = 'Directors'
+
+    id = db.Column(db.Integer, Sequence('directors_id_seq'), primary_key=True)
+    profileid = db.Column(db.Integer, db.ForeignKey('Profiles.id',ondelete="CASCADE"))
+    note = db.Column(db.Text())
+
+    def __init__(self, id, profileid, note):
+        self.id = id
+        self.profileid = profileid
+        self.note = note
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'profileid': self.profileid,
+            'note': self.note,
+        }
+
+
+class MovieDirector(db.Model):
+    __tablename__ = 'MovieDirectors'
+
+    id = db.Column(db.Integer, Sequence('moviedirectors_id_seq'), primary_key=True)
+    directorid = db.Column(db.Integer, db.ForeignKey('Directors.id',ondelete="CASCADE"))
+    movieid = db.Column(db.Integer, db.ForeignKey('Movies.id',ondelete="CASCADE"))
+
+    def __init__(self, id, directorid, movieid):
+        self.id = id
+        self.directorid = directorid
+        self.movieid = movieid
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'directorid': self.directorid,
+            'movieid': self.movieid,
+        }
+
+class Producer(db.Model):
+    __tablename__ = 'Producers'
+
+    id = db.Column(db.Integer, Sequence('producers_id_seq'), primary_key=True)
+    profileid = db.Column(db.Integer, db.ForeignKey('Profiles.id',ondelete="CASCADE"))
+    note = db.Column(db.Text())
+
+    def __init__(self, id, profileid, note):
+        self.id = id
+        self.profileid = profileid
+        self.note = note
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'profileid': self.profileid,
+            'note': self.note,
+        }
+
+class MovieProducer(db.Model):
+    __tablename__ = 'MovieProducers'
+
+    id = db.Column(db.Integer, Sequence('movieproducers_id_seq'), primary_key=True)
+    producerid = db.Column(db.Integer, db.ForeignKey('Producers.id',ondelete="CASCADE"))
+    movieid = db.Column(db.Integer, db.ForeignKey('Movies.id',ondelete="CASCADE"))
+
+    def __init__(self, id, producerid, movieid):
+        self.id = id
+        self.producerid = producerid
+        self.movieid = movieid
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'producerid': self.producerid,
+            'movieid': self.movieid,
+        }
